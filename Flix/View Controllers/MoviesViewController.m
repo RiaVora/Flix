94613a6239ce1feeb2766e7adbcddbb916778873
Ria Vora <riavora@fb.com>
2020-06-26 09:23:22 -0700

    added the details view to collection view. now, the user can click on a poster in collection view and see a detailed description of the movie!

--- Flix/View Controllers/MoviesViewController.m
+++ Flix/View Controllers/MoviesViewController.m
@@ -103,9 +103,6 @@
     cell.titleLabel.text = movie[@"title"];
     cell.descriptionLabel.text = movie[@"overview"];
     
-//    [cell.titleLabel sizeToFit];
-//    [cell.descriptionLabel sizeToFit];
-    
     NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
     NSString *posterURLString = movie[@"poster_path"];
     NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
